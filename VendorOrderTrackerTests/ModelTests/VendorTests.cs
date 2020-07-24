70e9674fb1885da1cea76cdbb051bc7f79292049
JohnNils Olson <johnnils@gmail.com>
2020-07-24 09:27:14 -0700

    add vendor class, vendor constructor test

---
+++ VendorOrderTrackerTests/ModelTests/VendorTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendorOrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace VendorOrderTracker.Tests
+{
+  [TestClass]
+  public class CategoryTest : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Category()
+    {
+      Vendor newVendor = new Vendor("test vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
