56b7dd69fb5710fedbaa7f1ceeb0cd5728591221
JohnNils Olson <johnnils@gmail.com>
2020-07-24 09:58:07 -0700

    add test for getall method

--- VendorOrderTrackerTests/ModelTests/VendorTests.cs
+++ VendorOrderTrackerTests/ModelTests/VendorTests.cs
@@ -37,5 +37,18 @@ namespace VendorOrderTracker.Tests
 
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Susie's Cafe";
+      string name02 = "Joe's Cafe";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+      List<Vendor> result = Vendor.GetAll();
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
