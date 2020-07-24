859dc7a4238a4aa7d6a3dd9d6ba9152576614d45
JohnNils Olson <johnnils@gmail.com>
2020-07-24 10:19:59 -0700

    add test for addorder method

--- VendorOrderTrackerTests/ModelTests/VendorTests.cs
+++ VendorOrderTrackerTests/ModelTests/VendorTests.cs
@@ -63,5 +63,19 @@ namespace VendorOrderTracker.Tests
 
       Assert.AreEqual(newVendor1, foundVendor);
     }
+    [TestMethod]
+    public void AddOrder_AssociatesOrderWithVendor_OrderList()
+    {
+      string description = "test order";
+      Order newOrder = new Order(description);
+      List<Order> newList = new List<Order> { newOrder };
+      string name = "Susie's Cafe";
+      Vendor newVendor = new Vendor(name);
+      newVendor.AddOrder(newOrder);
+
+      List<Order> result = newVendor.Orders;
+
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
